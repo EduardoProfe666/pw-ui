dda053383210443236d0cd2e0c70f649fa805946
Eduardo Alejandro González Martell <eduardoglez64377@gmail.com>
2024-11-23 11:12:08 -0500

    fix: protected routes

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,10 +14,16 @@ const authStore = useAuthStore()
 const route = useRoute();
 const router = useRouter();
 const hiddenRoutes = ['/login', '/forgot-password', '/reset-password', '/403'];
+const studentRoutes = ['/login', '/forgot-password', '/reset-password', '/403', '/', '/profile', '/profile/settings'];
 
 if(!authStore.isAuthenticated && !hiddenRoutes.includes(route.path)) {
   router.push('/login');
 }
+
+if(authStore.isAuthenticated && authStore.role === 'student' && !studentRoutes.includes(route.path)) {
+  router.push('/403');
+}
+
 const shouldShowNavbar = computed(() => {
   return authStore.isAuthenticated && !hiddenRoutes.includes(route.path);
 });
